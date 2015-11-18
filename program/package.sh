a980653ac0132ef183333314f461e25d4abd8792
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-11-18 10:35:36 +0100

    Präzisere Auswahl zu paketierender Dateien

--- program/package.sh
+++ program/package.sh
@@ -4,7 +4,9 @@ printf -v series 'uebung-%02d' "$1"
 series_file="pack/$series-abgabe.tar.bz2"
 cd "${0/*}" 2>&- || true
 
-find src doc "screenshot/$series" "spec/$series" run.sh \( -name __\* -prune -false \) -o \( -type f ! -name TODO\* -print0 \) | 
+find \
+  src data/DateInDays.txt data/January.txt doc run.sh "screenshot/$series" "spec/$series" \
+  \( -name __\* -prune -false \) -o \( -type f ! -name TODO\* -print0 \) |
 xargs -r -0 -- git ls-files -z -- |
 tar -vchaf "$series_file" --null -T -
 
