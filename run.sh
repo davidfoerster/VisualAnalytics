09b0a4755f82edc7b2a550034b2a013ac9c75ecd
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-11-20 21:11:53 +0100

    Fix typo in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -eu
 RUNSCRIPT="`readlink -e -- "$0"`"
 SCRIPTDIR="${RUNSCRIPT%/*}"
 SRCDIR="$SCRIPTDIR/src"
-[ $# -gt 0 ] || set -- "SCRIPTDIR/data/data-j-m"
+[ $# -gt 0 ] || set -- "$SCRIPTDIR/data/data-j-m"
 
 make -C "$SRCDIR" all
 exec python3.4 -O "$SRCDIR/main.py" "$@"
