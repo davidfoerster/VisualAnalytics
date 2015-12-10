39ef660383fcc3d886c28f6760fd3f81a9f46dc0
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-12-10 19:53:39 +0100

    Wähle gesamten Datensatz für Abgabe
    In der Auswertung gab es Punktabzug, weil die Zeitleiste anscheinend nur
    Januar bis März enthielt.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -eu
 RUNSCRIPT="`readlink -e -- "$0"`"
 SCRIPTDIR="${RUNSCRIPT%/*}"
 SRCDIR="$SCRIPTDIR/src"
-[ $# -gt 0 ] || set -- "$SCRIPTDIR/data/data-j-m"
+[ $# -gt 0 ] || set -- "$SCRIPTDIR/data/dust-2014.dat"
 
 make -C "$SRCDIR" all
 exec python3.4 -O "$SRCDIR/main.py" "$@"
