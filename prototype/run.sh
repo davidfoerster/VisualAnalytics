7c43f93154eca7f929f0438bd395cae45cdd04f9
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-12-21 09:14:17 +0100

    Verschiebe run.sh nach prototype/, wie gefordert

--- prototype/run.sh
+++ prototype/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 RUNSCRIPT="`readlink -e -- "$0"`"
-SCRIPTDIR="${RUNSCRIPT%/*}"
-SRCDIR="$SCRIPTDIR/src"
-[ $# -gt 0 ] || set -- "$SCRIPTDIR/data/dust-2014.dat"
+ROOTDIR="${RUNSCRIPT%/*}/.."
+SRCDIR="$ROOTDIR/src"
+[ $# -gt 0 ] || set -- "$ROOTDIR/data/dust-2014.dat"
 
 make -C "$SRCDIR" all
 exec python3.4 -O "$SRCDIR/main.py" "$@"
